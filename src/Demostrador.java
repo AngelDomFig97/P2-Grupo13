fc7c6e122b81e5980c3443c2a2ed58632e2500aa
adrisierra <adriansr2010@gmail.com>
2020-04-06 13:47:16 +0200

    Prueba de Adrian Sierra

--- src/Demostrador.java
+++ src/Demostrador.java
@@ -1,7 +1,7 @@
 public class Demostrador {
 
     public static void main(String[] args) {
-        System.out.println("VAMOS AMIGOS!!!!");
+        System.out.println("VAMOS Amigos, lo tengo!!!!");
     }
     
 }
