b32d06cf2ad33e7363cbf4353d17bf90daf00e2d
AngelDomFig97 <AngelDomFig97@users.noreply.github.com>
2020-04-07 13:07:11 +0200

    Añadir metodos de la clase web

--- src/Clases/Web.java
+++ src/Clases/Web.java
@@ -2,4 +2,29 @@ package Clases;
 
 public class Web {
     
+    private String mensajePopUp;
+
+    public String getMensajePopUp() {
+        return mensajePopUp;
+    }
+
+    public void setMensajePopUp(String mensajePopUp) {
+        this.mensajePopUp = mensajePopUp;
+    }
+    
+    public Usuario registrarse (){
+        return new Usuario();
+    }
+    
+    public Boolean iniciarSesion (){
+        return true;
+    }
+    
+    public Entrada mostrarEntradasPopulares (){
+        return new Entrada();
+    }
+    
+    public void mostrarPopUp (String mensaje){
+        
+    }
 }
