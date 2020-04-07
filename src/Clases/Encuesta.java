6eaa5f29842bb034bdafe6c3b2d051f81e115bfd
ariascary <caridadariaspradas3@gmail.com>
2020-04-07 13:33:46 +0200

    clase encuesta

--- src/Clases/Encuesta.java
+++ src/Clases/Encuesta.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
-/**
- *
- * @author Angel Dominguez
- */
+import java.util.ArrayList;
+
 public class Encuesta {
+    
+     private ArrayList<String> preguntasYrespuestas = new ArrayList<>(); 
+      
+     
+     
+     
+     
+     public Encuesta generarEncuesta(){
+         return null;
+        
+    }
+      
+    
+    
     
 }
