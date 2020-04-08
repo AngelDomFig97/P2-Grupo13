2ecc075a60c7660e2dea2fcc27de17c9342c85e6
AngelDomFig97 <AngelDomFig97@users.noreply.github.com>
2020-04-08 15:04:23 +0200

    Añadida clase web y borrado todo lo de Demostrador para dejarlo vacio

--- src/Clases/Web.java
+++ src/Clases/Web.java
@@ -1,8 +1,13 @@
 package Clases;
 
+import GestionBBDD.CRUD_Usuario;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Web {
     
     private String mensajePopUp;
+    public Date fecha;
 
     public String getMensajePopUp() {
         return mensajePopUp;
@@ -12,12 +17,22 @@ public class Web {
         this.mensajePopUp = mensajePopUp;
     }
     
-    public Usuario registrarse (){
-        return new Usuario();
-    }
-    
-    public Boolean iniciarSesion (){
+    public boolean registrarse (ArrayList<Usuario> listaUsuarios){
+        CRUD_Usuario.insertUsuario(listaUsuarios);
         return true;
+    }
+   
+    public Boolean iniciarSesion (String nick, String contraseña){
+        ArrayList<Usuario> listaUsuarios = CRUD_Usuario.selectUsuario();
+        for (Usuario usuarioActual : listaUsuarios){
+            if (usuarioActual.getNick().equals(nick)){
+                if (usuarioActual.getContraseña().equals(contraseña))
+                    return true;
+                else 
+                    return false;
+            }
+        }   
+        return false;
     }
     
     public Entrada mostrarEntradasPopulares (){
--- src/Demostrador.java
+++ src/Demostrador.java
@@ -6,23 +6,6 @@ import java.util.ArrayList;
 public class Demostrador {
 
     public static void main(String[] args) {
-        ArrayList<Usuario> listaUsuarios = new ArrayList<>();
-        Web pruebaWeb = new Web();
-        Usuario user = new Usuario();
-        user.setNombre("Angel");
-        user.setApellidos("Dominguez Figueros");
-        user.setNick("Gelote97");
-        user.setContraseña("manduca");
-        user.setEmail("a.dominguezf.2018@alumnos.urjc.es");
-        user.setTipoUsuario("Alumno");
-        user.setPenalizado(false);
-        listaUsuarios.add(user);
-        //pruebaWeb.registrarse(listaUsuarios);
         
-        Boolean iniciarSesion = pruebaWeb.iniciarSesion(user.getNick(), user.getContraseña());
-        if (iniciarSesion)
-            System.out.println("se ha iniciado sesion correctamente.");
-        else
-            System.out.println("la contraseña o el usuario introducidos son incorrectos.");
     }   
 }
