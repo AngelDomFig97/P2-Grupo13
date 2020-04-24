c04345679fef41c21424754b04ef49945d9b59ef
AngelDomFig97 <AngelDomFig97@users.noreply.github.com>
2020-04-24 12:39:48 +0200

    Probado el registro e inicio de sesion en la app.

--- src/Demostrador.java
+++ src/Demostrador.java
@@ -11,16 +11,19 @@ public class Demostrador {
     public static void main(String[] args) {
         ArrayList<Usuario> listaUsuarios = CRUD_Usuario.seleccionarUsuario();
         ArrayList<SubForo> listaSubForos = CRUD_SubForo.seleccionarSubForo();
+        String userActual, passActual;
+        userActual = "Alfonsot32";
+        passActual = "transistorSalvaje";
         Usuario usuario1 = new Usuario();
-        usuario1.setNombre("Angel");
-        usuario1.setApellidos("Dominguez Figueros");
-        usuario1.setNick("Gelote97");
-        usuario1.setContraseña("bocataDeAtun");
-        usuario1.setEmail("a.dominguezf.2018@alumnos.urcj.es");
-        usuario1.setTipoUsuario("Alumno");
+        usuario1.setNombre("Alfonso");
+        usuario1.setApellidos("Timón");
+        usuario1.setNick("Alfonsot32");
+        usuario1.setContraseña("transistorSalvaje");
+        usuario1.setEmail("a.timon@urcj.es");
+        usuario1.setTipoUsuario("Profesor");
         usuario1.setPenalizado(false);
         Web web1 = new Web();
-        //web1.registrarse(listaUsuarios, usuario1);
+        web1.registrarse(listaUsuarios, usuario1);
         Boolean inicioSesionCorrecto = web1.iniciarSesion("Gelote97", "bocataDeAtun");
         if(inicioSesionCorrecto)
             System.out.println("Se ha iniciado sesion correctamente");
