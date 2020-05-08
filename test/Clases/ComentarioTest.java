8b63b5446db9afd975f5ef0c066b4e2687129c11
marcoszas <marcoszas@users.noreply.github.com>
2020-05-08 11:34:25 +0200

    Probando librerias de test

---
+++ test/Clases/ComentarioTest.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Bugat
+ */
+public class ComentarioTest {
+    
+    public ComentarioTest() {
+    }
+
+    /**
+     * Test of getNombreUsuario method, of class Comentario.
+     */
+    @Test
+    public void testGetNombreUsuario() {
+    }
+
+    /**
+     * Test of setNombreUsuario method, of class Comentario.
+     */
+    @Test
+    public void testSetNombreUsuario() {
+    }
+
+    /**
+     * Test of getPuntuacion method, of class Comentario.
+     */
+    @Test
+    public void testGetPuntuacion() {
+    }
+
+    /**
+     * Test of setPuntuacion method, of class Comentario.
+     */
+    @Test
+    public void testSetPuntuacion() {
+    }
+
+    /**
+     * Test of getFechaCreacion method, of class Comentario.
+     */
+    @Test
+    public void testGetFechaCreacion() {
+    }
+
+    /**
+     * Test of setFechaCreacion method, of class Comentario.
+     */
+    @Test
+    public void testSetFechaCreacion() {
+    }
+
+    /**
+     * Test of getTexto method, of class Comentario.
+     */
+    @Test
+    public void testGetTexto() {
+    }
+
+    /**
+     * Test of setTexto method, of class Comentario.
+     */
+    @Test
+    public void testSetTexto() {
+    }
+
+    /**
+     * Test of getListaVotos method, of class Comentario.
+     */
+    @Test
+    public void testGetListaVotos() {
+    }
+
+    /**
+     * Test of setListaVotos method, of class Comentario.
+     */
+    @Test
+    public void testSetListaVotos() {
+    }
+
+    /**
+     * Test of getContestacionesComentarios method, of class Comentario.
+     */
+    @Test
+    public void testGetContestacionesComentarios() {
+    }
+
+    /**
+     * Test of addContestacionesComentarios method, of class Comentario.
+     */
+    @Test
+    public void testAddContestacionesComentarios() {
+    }
+
+    /**
+     * Test of contestarComentario method, of class Comentario.
+     */
+    @Test
+    public void testContestarComentario() {
+    }
+
+    /**
+     * Test of incrementarPuntuacion method, of class Comentario.
+     */
+    @Test
+    public void testIncrementarPuntuacion() {
+    }
+
+    /**
+     * Test of decrementarPuntuacion method, of class Comentario.
+     */
+    @Test
+    public void testDecrementarPuntuacion() {
+    }
+
+    /**
+     * Test of crearComentario method, of class Comentario.
+     */
+    @Test
+    public void testCrearComentario() {
+    }
+    
+}
