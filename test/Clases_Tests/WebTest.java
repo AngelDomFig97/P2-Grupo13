85e6868efdfae6c9a92889e09539ca8161e799b9
adrisierra <adriansr2010@gmail.com>
2020-05-09 13:55:22 +0200

    Move testweb a clases_test

--- test/Clases_Tests/WebTest.java
+++ test/Clases_Tests/WebTest.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Clases;
+package Clases_Tests;
 
+import Clases.Usuario;
+import Clases.Web;
 import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
